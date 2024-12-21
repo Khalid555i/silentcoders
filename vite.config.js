fff04e303c0aabd3181790d295255391aa1f282a
M.K. Khalid Mahamud <teamsilents@gmail.com>
2024-12-21 22:43:56 +0600

    add: initial files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/silentcoders/"
+  base: "/silentcoders/",
+  server: {
+        host: '192.168.1.117',
+        port: 5173
+  }
 })
